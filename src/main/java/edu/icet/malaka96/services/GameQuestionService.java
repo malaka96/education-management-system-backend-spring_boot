b7d5558c2f63f1299ade4f033c64d6d94bdf2849
malaka96 <madhubhashana655@gmail.com>
2026-01-06 23:16:43 +0530

    added game question service class

---
+++ src/main/java/edu/icet/malaka96/services/GameQuestionService.java
@@ -0,0 +1,27 @@
+package edu.icet.malaka96.services;
+
+import edu.icet.malaka96.models.GameQuestion;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameQuestionService {
+
+    public List<GameQuestion> getQuestions(){
+        return null;
+    }
+
+    public void addQuestion(GameQuestion gameQuestion){
+        //
+    }
+
+    public void updateQuestion(String id, GameQuestion gameQuestion){
+        //
+    }
+
+    public void deleteQuestion(String id){
+        //
+    }
+
+}
