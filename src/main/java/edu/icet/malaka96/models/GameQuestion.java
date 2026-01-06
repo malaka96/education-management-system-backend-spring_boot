fbe8d506f8345129c6f56ae03afb5ea311e6c859
malaka96 <madhubhashana655@gmail.com>
2026-01-06 23:14:02 +0530

    added lombok

--- src/main/java/edu/icet/malaka96/models/GameQuestion.java
+++ src/main/java/edu/icet/malaka96/models/GameQuestion.java
@@ -1,5 +1,12 @@
 package edu.icet.malaka96.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
 public class GameQuestion {
     private String id;
     private String title;
