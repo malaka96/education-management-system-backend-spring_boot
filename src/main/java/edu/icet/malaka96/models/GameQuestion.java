d4eb9bfbe9992974cf4b6aa5331a2dd197c7f5d5
malaka96 <madhubhashana655@gmail.com>
2026-01-06 23:12:16 +0530

    created game question model

---
+++ src/main/java/edu/icet/malaka96/models/GameQuestion.java
@@ -0,0 +1,9 @@
+package edu.icet.malaka96.models;
+
+public class GameQuestion {
+    private String id;
+    private String title;
+    private String question;
+    private String[] answers;
+    private String type;
+}
